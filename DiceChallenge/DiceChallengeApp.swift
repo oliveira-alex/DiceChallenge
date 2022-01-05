01d62d0ea5941c85dbc386b6cefa165394055b07
ALeX <alexdeoliveira.poli@gmail.com>
2022-01-05 13:55:08 -0300

    Initial Commit

---
+++ DiceChallenge/DiceChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiceChallengeApp.swift
+//  DiceChallenge
+//
+//  Created by Alex Oliveira on 05/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct DiceChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
